01be7e88c3df1361d91c3c9fb2c64c41ec47517d
Jeena <hello@jeena.net>
2020-08-12 22:26:38 +0200

    Create a GTK hello world application
    This commit sets everything up to be able to initialize a GTK
    application with a window and a button.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+extern crate gtk;
+extern crate gio;
+
+use gtk::prelude::*;
+use gio::prelude::*;
+
+use gtk::{Application, ApplicationWindow, Button};
+
+
 fn main() {
-    println!("Hello, world!");
+    
+    let application = Application::new(
+        Some("net.jeena.feedthemonkey"),
+        Default::default(),
+    ).expect("failed to initialize GTK application");
+
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::new(app);
+        window.set_title("FeedTheMonkey");
+        window.set_default_size(350, 70);
+
+        let button = Button::with_label("Click me!");
+        button.connect_clicked(|_| {
+            println!("Clicked!");
+        });
+        window.add(&button);
+
+        window.show_all();
+    });
+
+    application.run(&[]);
 }
