3f492b6160930220cc7d6ba83c394f39a4a032e8
Jeena <hello@jeena.net>
2021-05-24 22:10:23 +0200

    Fix problem with HDPI screens
    On high dpi screens the thml was shown way too small, this let's it
    scale properly.

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,7 @@
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
     app.setOrganizationName("Jeena");
     app.setOrganizationDomain("jeena.net");
